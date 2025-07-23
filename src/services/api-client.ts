92b6b668f85674432d981b9350a36129d1befcad
aidynprince <princeadjei231@outlook.com>
2025-07-23 10:51:05 +0000

    fixed cors error

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://www.freetogame.com/api",
+    baseURL: "https://proxy.corsfix.com/?https://www.freetogame.com/api",
 });
